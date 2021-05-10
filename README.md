93ba586c650585a801c3c579ba67d7b4510c4dd5
ronenk1 <ronenk1@rnd-hub.com>
2021-05-10 13:35:44 +0300

    chore(automation): implement test for ingestion process

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # automation-tests-3rd
-mc-3rd-automation-tests
+## mc-3rd-automation-tests - test docker
+### contents:
+1. 3rd tiles ingestion process
---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="mc-3rd-services-automation",
+    author="MC",
+    description="Map colonies 3rd services automation tests suite",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/MapColonies/automation-tests-3rd.git",
+    packages=setuptools.find_packages(),
+    install_requires=requirements,
+    use_scm_version=True,
+    setup_requires=['setuptools_scm'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
